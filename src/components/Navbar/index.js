ee348bf40a9e7e7bcde9b16cf9d9851c9f5ca1f5
Alicia Gorton <apersigehl@gmail.com>
2020-11-19 20:07:18 -0800

    deleted unused react files, started adding compnents

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,18 @@
+// import React, { useState, useEffect } from 'react';
+// import { Button } from './Button';
+// import { Link } from 'react-router-dom';
+
+function Navbar() {
+  return (
+   <>
+   <nav>
+       <div>
+           Hello
+       </div>
+   </nav>
+   </>
+  );
+}
+
+
+export default Navbar;
---
+++ src/components/pages/Login.js
@@ -0,0 +1,20 @@
+import Navbar from "../Navbar/index.js";
+import "./App.css";
+// import React, { useState, useEffect } from 'react';
+// import { Button } from './Button';
+// import { Link } from 'react-router-dom';
+
+function Login() {
+    return (
+     <>
+     <nav>
+         <div>
+             Hello
+         </div>
+     </nav>
+     </>
+    );
+  }
+  
+  
+  export default Login;
