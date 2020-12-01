069ea3be7225f5f074e635a4b43cdfb05ab0e70b
Alicia Gorton <apersigehl@gmail.com>
2020-11-30 18:22:51 -0800

    added additional pages to navbar

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React from 'react';
+import {Route} from 'react-router-dom';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import './App.css';
-import Home from "./components/";
+import Home from "./pages/Homepage/index";
+import Create from './pages/Create/index';
 
 
 function App() {
   return (
-    <>
-   <CssBaseline />
-    <Home />
-    </>
+    <div>
+      <CssBaseline />
+        {/* <Route path='/' exact component={Login} /> */}
+        <Route path='/' exact component={Home} />
+        <Route path='/create' exact component={Create} />
+        
+    </div>
   );
 }
 
