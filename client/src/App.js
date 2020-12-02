a80edbe5350354fa863ea9461b7c10e1c4a5d980
Kristin Brumfield <kristinbrumfield@Kbrummy.local>
2020-12-01 20:17:20 -0800

    Set up the Login page, but have not linked it yet.

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,19 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  //Redirect,
+} from "react-router-dom";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import "./App.css";
 import Dashboard from "./pages/Dashboard/index";
-import Create from './pages/CreateNewPDF/index';
-import Profile from './pages/Profile/index';
-import Saved from './pages/Saved/index';
-import NoMatch from './pages/NoMatch/index';
-import Footer from './components/Footer';
-// import Login form './pages/Login/index';
+import Create from "./pages/CreateNewPDF/index";
+import Profile from "./pages/Profile/index";
+import Saved from "./pages/Saved/index";
+import NoMatch from "./pages/NoMatch/index";
+import Footer from "./components/Footer";
+import Login from "./pages/Login/index";
 // import Register from './pages/Register/index';
 
 function App() {
@@ -16,43 +21,44 @@ function App() {
     <Router>
       <div>
         <CssBaseline />
-            <Switch>
-              {/* <Route exact path={"/"}>
-                <Login />
-              </Route>
-              <Route path={"/login"}>
+        <Switch>
+          <Route exact path={"/login"}>
+            <Login />
+          </Route>
+          {/* <Route path={"/login"}>
                 <Redirect to="/register" />
               </Route>
               <Route  path={"/register"}>
                 <Redirect to="/dashboard" />
               </Route> */}
-              <Route exact path={"/"}>
-                <Dashboard />
-              </Route>
-              <Route exact path={"/profile"}>
-                <Profile />
-              </Route>
-              <Route exact path={"/saved"}>          
-                <Saved />
-              </Route>
-              <Route exact path={"/create"}>          
-                <Create />
-              </Route>
-              <Route path="*">
-                <NoMatch />
-              </Route>
-           </Switch>
-           <Footer />
+          <Route exact path={"/"}>
+            <Dashboard />
+          </Route>
+          <Route exact path={"/profile"}>
+            <Profile />
+          </Route>
+          <Route exact path={"/saved"}>
+            <Saved />
+          </Route>
+          <Route exact path={"/create"}>
+            <Create />
+          </Route>
+          <Route path="*">
+            <NoMatch />
+          </Route>
+        </Switch>
+        <Footer />
       </div>
     </Router>
-    );
+  );
 }
 
-
 // eslint-disable-next-line no-lone-blocks
-{/* <Route   component={Dashboard} />
+{
+  /* <Route   component={Dashboard} />
 <Route path='/profile' exact component={Profile} />
 <Route path='/create' exact component={Create} /> 
-<Route path='/saved' exact component={Saved} /> */}
+<Route path='/saved' exact component={Saved} /> */
+}
 
 export default App;
--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,77 +1,89 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/styles';
-import {BottomNavigation, BottomNavigationAction, Typography, Card} from '@material-ui/core';
-import GitHubIcon from '@material-ui/icons/GitHub';
+import React from "react";
+import { makeStyles } from "@material-ui/styles";
+import {
+  BottomNavigation,
+  BottomNavigationAction,
+  Typography,
+  Card,
+} from "@material-ui/core";
+import GitHubIcon from "@material-ui/icons/GitHub";
 
 const useStyles = makeStyles({
-    root: {
-        background: "#1d242b",
-        "& .MuiBottomNavigationAction-root": {
-            minWidth: 0,
-            maxWidth: 300
-        },
-        "& .MuiSvgIcon-root": {
-            fill: "white",
-            fontSize: "1.8rem",
-        }
+  root: {
+    background: "#1d242b",
+    "& .MuiBottomNavigationAction-root": {
+      minWidth: 0,
+      maxWidth: 300,
     },
-    footerContainer: {
-        backgroundColor: "#1d242b",
-        color: "white",
-        padding: "1rem",
-        height: "auto",
+    "& .MuiSvgIcon-root": {
+      fill: "white",
+      fontSize: "1.8rem",
     },
-    subHeadingTwo: {
-        textAlign: "center",
-        padding: "0.5rem",
-    }
-
-})
+  },
+  footerContainer: {
+    backgroundColor: "#1d242b",
+    color: "white",
+    padding: "1rem",
+    height: "auto",
+  },
+  subHeadingTwo: {
+    textAlign: "center",
+    padding: "0.5rem",
+  },
+});
 
 const Footer = () => {
+  const classes = useStyles();
 
-    const classes = useStyles();
-
-    return(
-        <div className={classes.footerContainer}>
-            <Typography className={classes.subHeadingTwo}>Created & Designed by: </Typography>
-            <BottomNavigation width="auto" style={{background: "#1d242b"}}>
-            <Typography>Ian Astesana
-                <BottomNavigationAction
-                    href="https://github.com/Greezon" target="_blank"
-                    className={classes.root}
-                    style={{padding: 0}}
-                    icon={<GitHubIcon/>}
-                />
-            </Typography>
-            <Typography>Kristin Brumfield
-            <BottomNavigationAction
-                href="https://github.com/kbrummy" target="_blank"
-                className={classes.root}
-                style={{padding: 0}}
-                icon={<GitHubIcon/>}
-            />
-            </Typography>
-            <Typography>Alan Fernandes
-            <BottomNavigationAction
-                href="https://github.com/anoblefernandes" target="_blank"
-                className={classes.root}
-                style={{padding: 0}}
-                icon={<GitHubIcon/>}
-            />
-            </Typography>
-            <Typography>Alicia Gorton
-            <BottomNavigationAction
-                href="https://github.com/aliciagorton" target="_blank"
-                className={classes.root}
-                style={{padding: 0}}
-                icon={<GitHubIcon/>}
-            />
-            </Typography>
-        </BottomNavigation>
-        </div>
-       
-    );
-}
+  return (
+    <div className={classes.footerContainer}>
+      <Typography className={classes.subHeadingTwo}>
+        Created & Designed by:{" "}
+      </Typography>
+      <BottomNavigation width="auto" style={{ background: "#1d242b" }}>
+        <Typography>
+          Ian Astesana
+          <BottomNavigationAction
+            href="https://github.com/Greezon"
+            target="_blank"
+            className={classes.root}
+            style={{ padding: 0 }}
+            icon={<GitHubIcon />}
+          />
+        </Typography>
+        <Typography>
+          Kristin Brumfield
+          <BottomNavigationAction
+            href="https://github.com/kbrummy"
+            target="_blank"
+            className={classes.root}
+            style={{ padding: 0 }}
+            icon={<GitHubIcon />}
+          />
+        </Typography>
+        <Typography>
+          Alan Fernandes
+          <BottomNavigationAction
+            href="https://github.com/anoblefernandes"
+            target="_blank"
+            className={classes.root}
+            style={{ padding: 0 }}
+            icon={<GitHubIcon />}
+          />
+        </Typography>
+        <Typography>
+          Alicia Gorton
+          <BottomNavigationAction
+            href="https://github.com/aliciagorton"
+            target="_blank"
+            className={classes.root}
+            style={{ padding: 0 }}
+            icon={<GitHubIcon />}
+          />
+        </Typography>
+      </BottomNavigation>
+    </div>
+  );
+};
 
 export default Footer;
