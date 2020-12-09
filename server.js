7b54b3112f9bcd47ae2558191b28e07d56ea02b2
Alicia Gorton <apersigehl@gmail.com>
2020-12-08 18:46:28 -0800

    changed dependencies added and  eliminated duplicates

--- server.js
+++ server.js
@@ -3,34 +3,42 @@ const mongoose = require("mongoose");
 const express = require("express");
 const cors = require('cors');
 const passport = require("passport");
-const passportLocal = require("passport-local").Strategy;
+//const passportLocal = require("passport-local").Strategy;
 const cookieParser = require("cookie-parser");
-const bcrypt = require("bcryptjs");
+//const bcrypt = require("bcryptjs");
 const session = require("express-session");
-const bodyParser = require("body-parser");
+//const bodyParser = require("body-parser");
 const routes = require("./routes");
+require('./models/user');
+
 const app = express();
+require('./config/passport')(passport);
+
 const PORT = process.env.PORT || 3001;
 
-const User = require("./user");
-const routes = require("./routes");
+//const User = require("./user");
+//const routes = require("./routes");
 
 //----------------------------------------- END OF IMPORTS---------------------------------------------------
 mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/project3",
   // "mongodb+srv://{Place Your Username Here!}:{Place Your Password Here!}@cluster0-q9g9s.mongodb.net/test?retryWrites=true&w=majority",
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-  },
-  () => {
-    console.log("Mongoose Is Connected");
+    useCreateIndex: true,
+    useFindAndModify: false
   }
 );
 
+mongoose.connection.on('connected', () => {
+  console.log('Mongoose is connected.');
+})
+
 // Middleware
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
     origin: "http://localhost:3000", // <-- location of the react app were connecting to
@@ -47,43 +55,43 @@ app.use(
 app.use(cookieParser("secretcode"));
 app.use(passport.initialize());
 app.use(passport.session());
-require("./passportConfig")(passport);
+//require("./passportConfig")(passport);
 
 //----------------------------------------- END OF MIDDLEWARE---------------------------------------------------
 
 // Routes
-app.post("/login", (req, res, next) => {
-  passport.authenticate("local", (err, user, info) => {
-    if (err) throw err;
-    if (!user) res.send("No User Exists");
-    else {
-      req.logIn(user, (err) => {
-        if (err) throw err;
-        res.send("Successfully Authenticated");
-        console.log(req.user);
-      });
-    }
-  })(req, res, next);
-});
-app.post("/register", (req, res) => {
-  User.findOne({ username: req.body.username }, async (err, doc) => {
-    if (err) throw err;
-    if (doc) res.send("User Already Exists");
-    if (!doc) {
-      const hashedPassword = await bcrypt.hash(req.body.password, 10);
+// app.post("/login", (req, res, next) => {
+//   passport.authenticate("local", (err, user, info) => {
+//     if (err) throw err;
+//     if (!user) res.send("No User Exists");
+//     else {
+//       req.logIn(user, (err) => {
+//         if (err) throw err;
+//         res.send("Successfully Authenticated");
+//         console.log(req.user);
+//       });
+//     }
+//   })(req, res, next);
+// });
+// app.post("/register", (req, res) => {
+//   User.findOne({ username: req.body.username }, async (err, doc) => {
+//     if (err) throw err;
+//     if (doc) res.send("User Already Exists");
+//     if (!doc) {
+//       const hashedPassword = await bcrypt.hash(req.body.password, 10);
 
-      const newUser = new User({
-        username: req.body.username,
-        password: hashedPassword,
-      });
-      await newUser.save();
-      res.send("User Created");
-    }
-  });
-});
-app.get("/user", (req, res) => {
-  res.send(req.user); // The req.user stores the entire user that has been authenticated inside of it.
-});
+//       const newUser = new User({
+//         username: req.body.username,
+//         password: hashedPassword,
+//       });
+//       await newUser.save();
+//       res.send("User Created");
+//     }
+//   });
+// });
+// app.get("/user", (req, res) => {
+//   res.send(req.user); // The req.user stores the entire user that has been authenticated inside of it.
+// });
 //----------------------------------------- END OF ROUTES---------------------------------------------------
 //Start Server
 // Start the API server
