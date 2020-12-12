597f57df44836f2ba678e3df388f12a9eaf4af54
Alan Fernandes <anoblefernandes@gmail.com>
2020-12-12 10:10:27 -0800

    Updated save button to store on the data base.

--- client/src/components/Button/SaveProfileBTN.js
+++ client/src/components/Button/SaveProfileBTN.js
@@ -3,6 +3,7 @@ import Button from "@material-ui/core/Button";
 import { Link } from "react-router-dom";
 import { makeStyles } from "@material-ui/core/styles";
 import SaveIcon from "@material-ui/icons/Save";
+import axios from "axios";
 
 const useStyles = makeStyles((theme) => ({
   button: {
@@ -24,6 +25,9 @@ export default function SaveProfileBTN() {
         startIcon={<SaveIcon />}
         component={Link}
         to="/saved"
+        onClick={(e) => {
+          e.preventDefault()
+        axios.post("api/profile",{})}}
       >
         Save
       </Button>
--- routes/api/profile.js
+++ routes/api/profile.js
@@ -8,9 +8,11 @@ router.route("/")
 
 // Matches with "/api/profile/:id"
 router
-  .route("/:id")
+  .route("/")
 //   .get(profileController.findById)
 //   .put(profileController.update)
 //   .delete(profileController.remove);
+.post(profileController.create)
+
 
 module.exports = router;
