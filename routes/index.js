5068720daa277dce438336348ce6e01e89d4c37d
Alicia Gorton <apersigehl@gmail.com>
2020-12-30 11:48:37 -0800

    cleaning up router

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,12 @@
-const path = require("path");
+// const path = require("path");
 const router = require("express").Router();
 const apiRoutes = require("./api");
 const authRoutes = require("./api/auth");
+// const profileRoutes = require("./api/profile");
 
 // API Routes
 router.use("/api", apiRoutes);
 router.use("/auth", authRoutes);
-
-// If no API routes are hit, send the React app
-router.use(function(req, res) {
-  res.sendFile(path.join(__dirname, "../client/build/index.html"));
-});
+// router.use("/profile", profileRoutes);
 
 module.exports = router;
