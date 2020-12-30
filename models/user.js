b363d761395e7c17844b7a18499db60a4dac1f35
Alicia Gorton <apersigehl@gmail.com>
2020-12-30 12:05:36 -0800

    adding required true to all fields in schema, and add password field to schema

--- models/user.js
+++ models/user.js
@@ -2,50 +2,44 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const passportLocalMongoose = require('passport-local-mongoose');
 const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
 
-//  || new mongoose.Schema
+const hashPassword = (password) => {
+  const saltRounds = 5;
+  const salt = bcrypt.genSaltSync(saltRounds);
+  return bcrypt.hashSync(password, salt);
+}
+
 const UserSchema = new Schema({
 
   first_name: {
     type: String,
     trim: true,
-    required: "First Name is Required"
+    required: [true, "First Name is Required"]
   },
   last_name: {
-      type: String,
-      trim: true,
-      required: "Last Name is Required"
-    },
+    type: String,
+    trim: true,
+    required: [true, "Last Name is Required"]
+  },
   email_address: {
-      type: String,
-      unique: true,
-      match: [/.+@.+\..+/, "Please enter a valid e-mail address"]
-    },
-
+    type: String,
+    unique: true,
+    match: [/.+@.+\..+/, "Please enter a valid e-mail address"]
+  },
+    password: {
+    type: String,
+    required: [true, 'Password required']
+  }
 });
 
-UserSchema.methods.validPassword = function(password) {
-    return bcrypt.compareSync(password, this.hash);
-}
+UserSchema.pre('save', function (next) {
+  if (this.password) {
+    this.password = hashPassword(this.password)
+  }
+  next()
+});
 
-UserSchema.methods.setPassword = function(password) {
-    this.hash = bcrypt.hashSync(password, 10);
-}
-
-UserSchema.methods.generateJWT = function() {
-    const today = new Date();
-    const exp = new Date(today);
-    exp.setDate(today.getDate() + 60);
-    return jwt.sign({
-        _id: this._id,
-        exp: parseInt(exp.getTime() / 1000)
-    }, process.env.jwtSecret);
-}
-
-// OR THIS 
 UserSchema.plugin(passportLocalMongoose, { usernameField : 'email_address' });
 const User = mongoose.model("User", UserSchema);
 
 module.exports = User;
-// module.exports = mongoose.model('Users', UserSchema);
