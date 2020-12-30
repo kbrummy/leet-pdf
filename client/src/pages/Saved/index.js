d37896861e5087cf81f161c0cc968a7f7719a010
Alicia Gorton <apersigehl@gmail.com>
2020-12-30 13:46:29 -0800

    cleaned up file, removed hardcoding from fetch request, made path relative

--- client/src/pages/Saved/index.js
+++ client/src/pages/Saved/index.js
@@ -1,22 +1,21 @@
 import React from 'react';
-import { Typography, Card } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
+import { Typography, Card, makeStyles } from '@material-ui/core';
 import Navbar from '../../components/Navbar';
 import Sidebar from "../Sidebar";
 
 const useStyles = makeStyles((theme) => ({
-    mainContainer: {
-        backgroundColor: "#dee3e2",
-        padding: "3rem",
-    },
-    heading: {
-        fontSize: "50px",
-        fontweight: "bold",
-        color: "#2e3236",
-        textAlign: "center",
-        paddding:"1rem",
-        background: "#c0c2c4",
-    },
+  mainContainer: {
+      backgroundColor: "#dee3e2",
+      padding: "3rem",
+  },
+  heading: {
+      fontSize: "50px",
+      fontweight: "bold",
+      color: "#2e3236",
+      textAlign: "center",
+      paddding:"1rem",
+      background: "#c0c2c4",
+  },
 }));
 
 const Saved = () => {
@@ -26,20 +25,20 @@ const Saved = () => {
     const handleClientInput = (e) => {};
 
     React.useEffect(() => {
-        const url = new URL("https://leet-pdf.herokuapp.com/");
-        // url.port = 3001;
+      let state = this.state;
+      const url = new URL(process.env.SERVER_URL);
         url.pathname = "/api/profile";
     
-        fetch(url.href, {
-          method: "GET",
+      fetch(url.href, {
+        method: "GET",
+      })
+        .then((res) => {
+          res.json().then((clients) => {
+            setState({ ...state, clients : clients });
+          });
         })
-          .then((res) => {
-            res.json().then((clients) => {
-              setState({ ...state, clients : clients });
-            });
-          })
-          .catch((error) => alert(error.message));
-      }, []);
+        .catch((error) => alert(error.message));
+    }, []);
 
     return (
         <div>
